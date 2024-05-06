e1ba0ccd4ff4e8cd22a68281a27838f7aabbc3d5
afcasco <7701272+afcasco@users.noreply.github.com>
2024-05-06 11:26:27 +0200

    update with latests work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {HeroesComponent} from "./heroes/heroes.component";
+import {MessagesComponent} from "./messages/messages.component";
+import {HttpClientModule} from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesComponent, MessagesComponent, RouterLink, HttpClientModule, HttpClientInMemoryWebApiModule],
+  g
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = 'Tour of Heroes';
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from "../hero";
+import {NgFor, NgIf, UpperCasePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {HeroService} from "../hero.service";
+import {HeroDetailComponent} from "../hero-detail/hero-detail.component";
+import {MessageService} from "../message.service";
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [
+    UpperCasePipe,
+    FormsModule,
+    NgFor,
+    NgIf,
+    HeroDetailComponent,
+    RouterLink
+  ],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+}
