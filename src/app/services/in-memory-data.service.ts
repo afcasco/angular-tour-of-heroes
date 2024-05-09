3dc3c24375109e9ea9444f057c762021a6f0fbb0
afcasco <7701272+afcasco@users.noreply.github.com>
2024-05-10 01:58:53 +0200

    format code

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -8,15 +8,19 @@ import {Hero} from '../model/hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      {id: 12, name: 'Dr. Nice',image:'https://superherotar.framiq.com/assets/examples/superherotar00.png'},
-      {id: 13, name: 'Bombasto',image:'https://superherotar.framiq.com/assets/examples/superherotar01.png'},
-      {id: 14, name: 'Celeritas',image:'https://superherotar.framiq.com/assets/examples/superherotar02.png'},
-      {id: 15, name: 'Magneta',image:'https://superherotar.framiq.com/assets/examples/superherotar03.png'},
-      {id: 16, name: 'RubberMan',image:'https://superherotar.framiq.com/assets/examples/superherotar04.png'},
-      {id: 17, name: 'Dynama',image:'https://superherotar.framiq.com/assets/examples/superherotar05.png'},
-      {id: 18, name: 'Dr. IQ',image: 'https://superherotar.framiq.com/assets/examples/superherotar06.png'},
-      {id: 19, name: 'Magma',image:'https://superherotar.framiq.com/assets/examples/superherotar07.png'},
-      {id: 20, name: 'Tornado',image:'https://img.freepik.com/premium-photo/3d-cat-avatar-online-games-web-account-avatar_147351-46.jpg'}
+      {id: 12, name: 'Dr. Nice', image: 'https://superherotar.framiq.com/assets/examples/superherotar00.png'},
+      {id: 13, name: 'Bombasto', image: 'https://superherotar.framiq.com/assets/examples/superherotar01.png'},
+      {id: 14, name: 'Celeritas', image: 'https://superherotar.framiq.com/assets/examples/superherotar02.png'},
+      {id: 15, name: 'Magneta', image: 'https://superherotar.framiq.com/assets/examples/superherotar03.png'},
+      {id: 16, name: 'RubberMan', image: 'https://superherotar.framiq.com/assets/examples/superherotar04.png'},
+      {id: 17, name: 'Dynama', image: 'https://superherotar.framiq.com/assets/examples/superherotar05.png'},
+      {id: 18, name: 'Dr. IQ', image: 'https://superherotar.framiq.com/assets/examples/superherotar06.png'},
+      {id: 19, name: 'Magma', image: 'https://superherotar.framiq.com/assets/examples/superherotar07.png'},
+      {
+        id: 20,
+        name: 'Tornado',
+        image: 'https://img.freepik.com/premium-photo/3d-cat-avatar-online-games-web-account-avatar_147351-46.jpg'
+      }
     ];
     return {heroes};
   }
